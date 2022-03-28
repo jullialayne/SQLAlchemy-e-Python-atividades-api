1c780881d2bd5306e5f41c5cf99b4b9f3b857b50
jullialayne <83183478+jullialayne@users.noreply.github.com>
2022-03-28 12:56:13 -0300

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,31 +1,32 @@
 from models import Pessoas,db_session
 
 #Insere dados na tabela pessoa
-def insere_pessoas():
-    pessoa = Pessoas(nome='Layne', idade=22)
-    print(pessoa)
+def insere_pessoas(pessoa):
     pessoa.save()
 
 #Consulta dados na tabela pessoa
 def consulta_pessoas():
     pessoas = Pessoas.query.all()
     print(pessoas)
-    #pessoa = Pessoas.query.filter_by(nome='Jullia').first()
-    #print(pessoa.idade)
 
 #Altera dados na tabela pessoa
-def altera_pessoa():
-    pessoa = Pessoas.query.filter_by(nome='Layne').first()
-    pessoa.nome='Junia'
+def altera_pessoa(pessoa,nome1):
+    pessoa = Pessoas.query.filter_by(nome=pessoa.nome).first()
+    pessoa.nome=nome1
     pessoa.save()
 
 #Exclui dados na tabela pessoa
-def exclui_pessoa():
-    pessoa = Pessoas.query.filter_by(nome='Junia').first()
+def exclui_pessoa(nome1):
+    pessoa = Pessoas.query.filter_by(nome=nome1).first()
     pessoa.delete()
 
 if __name__ == '__main__':
-    #insere_pessoas()
-    #altera_pessoa()
-    exclui_pessoa()
+
+    #simulaçoes
+    pessoa = Pessoas(nome='Layne', idade=22)
+    nome1='Junia'
+    
+    insere_pessoas(pessoa)
+    altera_pessoa(pessoa,nome1)
+    exclui_pessoa(nome1)
     consulta_pessoas()
